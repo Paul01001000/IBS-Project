6c5bfa89fa82804e9ea62fcdc54ec9c96543870b
Paul01001000 <max.kraft363@gmail.com>
2024-12-10 20:11:51 +0100

    add function

--- app.py
+++ app.py
@@ -11,10 +11,13 @@ def root():
 
 @app.route("/login")
 def login():
+    db.log_out
     return render_template("login.html")
 
 @app.route("/main", methods = ["POST"])
 def main():
+    if not db.get_user:
+        redirect("login")
     res = request.form
     if db.verify_user(res["name"],res["password"]):
         return render_template("main.html", 
@@ -35,6 +38,13 @@ def register():
         return redirect("login")
     else:
         return render_template("register.html")
+    
+@app.route("/patienten", methods = ["POST"])
+def clients():
+    client_data = db.get_clients_json
+    return render_template("clients.html",clients=client_data)
+    
+
 
 if __name__ == "__main__":
     app.run()
