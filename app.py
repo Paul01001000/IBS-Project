15cfb55abf98e1565c2b9147d6a73bcd14de3646
Paul01001000 <max.kraft363@gmail.com>
2024-12-11 16:08:58 +0100

    add functions

--- app.py
+++ app.py
@@ -14,10 +14,13 @@ def login():
     db.log_out
     return render_template("login.html")
 
-@app.route("/main", methods = ["POST"])
+@app.route("/main", methods = ["GET","POST"])
 def main():
     if not db.get_user:
         redirect("login")
+    if request.method == "GET":
+        return render_template("main.html", 
+                                name = db.get_user)
     res = request.form
     if db.verify_user(res["name"],res["password"]):
         return render_template("main.html", 
@@ -39,11 +42,29 @@ def register():
     else:
         return render_template("register.html")
     
-@app.route("/patienten", methods = ["POST"])
+@app.route("/patienten", methods = ["GET"])
 def clients():
+    if not db.get_user:
+        return redirect("login")
     client_data = db.get_clients_json
     return render_template("clients.html",clients=client_data)
     
+@app.route("/patient", methods = ["GET"])
+def client():
+    if not db.get_user:
+        return redirect("login")
+    res = request.args
+    this_client_data = db.get_client_json(res["id"])
+    return render_template("client_setting.html",client=this_client_data)
+
+@app.route("/patienten/neu", methods = ["POST"])
+def new_client():
+    if not db.get_user:
+        return redirect("login")
+    res = request.form
+    if not res:
+        return ""
+    return redirect("patienten")
 
 
 if __name__ == "__main__":
